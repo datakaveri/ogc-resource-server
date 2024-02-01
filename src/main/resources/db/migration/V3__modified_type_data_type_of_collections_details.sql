7f678245d8d0be020d9c63164ade467b73cfe492
Tanvi Prasad <tanvi.prasad@datakaveri.org>
2024-02-01 15:04:30 +0530

    collection_details data type modified

---
+++ src/main/resources/db/migration/V3__modified_type_data_type_of_collections_details.sql
@@ -0,0 +1,15 @@
+CREATE TYPE item_type AS ENUM ('OGC_FEATURES', 'OGC_TILES', 'STAC');
+
+ALTER TABLE collections_details ADD COLUMN temp_item_type item_type;
+
+UPDATE collections_details
+SET item_type = CASE
+    WHEN type = 'Feature' THEN 'OGC_FEATURES'::item_type
+    WHEN type = 'Tiles' THEN 'OGC_TILES'::item_type
+    WHEN type = 'Stac' THEN 'STAC'::item_type
+    ELSE NULL
+END;
+
+ALTER TABLE collections_details DROP COLUMN type;
+
+ALTER TABLE collections_details RENAME COLUMN temp_item_type TO type;
