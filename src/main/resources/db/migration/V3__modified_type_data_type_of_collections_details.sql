2549886bb99266f9ff3e6b1cd9407449a44afd8c
Tanvi Prasad <tanvi.prasad@datakaveri.org>
2024-02-01 16:53:01 +0530

    updated collection_details schema

--- src/main/resources/db/migration/V3__modified_type_data_type_of_collections_details.sql
+++ src/main/resources/db/migration/V3__modified_type_data_type_of_collections_details.sql
@@ -1,11 +1,11 @@
-CREATE TYPE item_type AS ENUM ('OGC_FEATURES', 'OGC_TILES', 'STAC');
+CREATE TYPE item_type AS ENUM ('FEATURE', 'MAP', 'STAC');
 
 ALTER TABLE collections_details ADD COLUMN temp_item_type item_type NOT NULL;
 
 UPDATE collections_details
 SET item_type = CASE
-    WHEN type = 'Feature' THEN 'OGC_FEATURES'::item_type
-    WHEN type = 'Tiles' THEN 'OGC_TILES'::item_type
+    WHEN type = 'Feature' THEN 'FEATURE'::item_type
+    WHEN type = 'Tiles' THEN 'MAP'::item_type
     WHEN type = 'Stac' THEN 'STAC'::item_type
     ELSE NULL
 END;
---
+++ src/main/resources/db/migration/V4__license_column_added_in_collections_details.sql
@@ -0,0 +1,1 @@
+ALTER TABLE collections_details ADD COLUMN license VARCHAR(255);
