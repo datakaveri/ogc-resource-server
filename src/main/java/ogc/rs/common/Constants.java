acbfdf8713dbbbf5d75d9833f955673cd81a7165
Gopal Mahajan <gopalmit2601@gmail.com>
2024-04-08 12:12:44 +0530

    refactor

--- src/main/java/ogc/rs/common/Constants.java
+++ src/main/java/ogc/rs/common/Constants.java
@@ -11,8 +11,8 @@ public class Constants {
     public static final String METERING_SERVICE_ADDRESS = "ogc.rs.metering.service";
     public static final String PROCESSING_SERVICE_ADDRESS = "ogc.rs.processes.service";
     public static final String JOBS_SERVICE_ADDRESS = "ogc.rs.jobs.service";
-  public static final String DEFAULT_SERVER_CRS = "http://www.opengis.net/def/crs/OGC/1.3/CRS84";
-    public static final String UUID_REGEX = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4  }-[0-9a-f]{12}$";
+   public static final String DEFAULT_SERVER_CRS = "http://www.opengis.net/def/crs/OGC/1.3/CRS84";
+    public static final String UUID_REGEX = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$";
 
   public static final String CATALOGUE_SERVICE_ADDRESS = "ogc.rs.catalog.service";
   public static final String EPOCH_TIME = "epochTime";
