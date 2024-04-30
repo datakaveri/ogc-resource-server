d3cb86d7d35ceab0c1db3fce342e0801d463e55e
Tanvi Prasad <tanvi.prasad@datakaveri.org>
2024-04-30 16:35:11 +0530

    fakeTokenBuilder fixes

--- src/test/java/ogc/rs/util/FakeTokenBuilder.java
+++ src/test/java/ogc/rs/util/FakeTokenBuilder.java
@@ -1,9 +1,13 @@
 package ogc.rs.util;
 
-import com.nimbusds.jose.*;
+import com.nimbusds.jose.JOSEObjectType;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
 import com.nimbusds.jose.crypto.ECDSASigner;
 import com.nimbusds.jose.jwk.ECKey;
 import com.nimbusds.jose.jwk.PasswordLookup;
+import com.nimbusds.jose.util.JSONObjectUtils;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import io.vertx.core.json.JsonArray;
@@ -22,15 +26,18 @@ public class FakeTokenBuilder {
   private static final Logger LOGGER = LogManager.getLogger(FakeTokenBuilder.class);
 
   private String iid;
-  private JsonObject cons;
-  private String sub;
-  private String aud;
+  private JsonObject cons = new JsonObject();
+  private UUID sub = UUID.randomUUID();
+  private static String aud;
   private String role;
   private static JWSSigner signer;
   private UUID did;
   private String drl;
 
   private UUID rg;
+  private static final String iss = "fakeauth.ugix.io";
+
+  // Static initialization block for setting up the signer and audience
 
   static {
     KeyStore keyStore = null;
@@ -50,68 +57,123 @@ public class FakeTokenBuilder {
       LOGGER.debug("jwkSET" + jwkSet.getKeys().get(0).toECKey().toECPrivateKey());
       ECKey ecPrivateKey = (ECKey) jwkSet.getKeys().get(0);
       signer = new ECDSASigner(ecPrivateKey);
+      String configFilePath = "secrets/configs/dev.json";
+      byte[] jsonData = Files.readAllBytes(Paths.get(configFilePath));
+      JsonObject configJson = new JsonObject(new String(jsonData, "UTF-8"));
+      JsonArray modules = configJson.getJsonArray("modules");
+      for (Object module : modules) {
+        if (module instanceof JsonObject) {
+          JsonObject moduleJson = (JsonObject) module;
+          if ("ogc.rs.authenticator.AuthenticationVerticle".equals(moduleJson.getString("id"))
+              && moduleJson.containsKey("audience")) {
+            aud = moduleJson.getString("audience");
+            LOGGER.debug("Audience Value : {} ", aud);
+            break;
+          }
+        }
+      }
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
   }
 
+  /**
+   * Sets the 'cons' claim for the JWT token.
+   *
+   * @param cons JsonObject representing the 'constraints'
+   * @return FakeTokenBuilder instance
+   */
   public FakeTokenBuilder withCons(JsonObject cons) {
     this.cons = cons;
     return this;
   }
 
-  public FakeTokenBuilder withSub(String sub) {
+  /**
+   * Sets the 'sub' (subject) claim for the JWT token.
+   *
+   * @param sub UUID representing the 'sub' claim
+   * @return FakeTokenBuilder instance
+   */
+  public FakeTokenBuilder withSub(UUID sub) {
     this.sub = sub;
     return this;
   }
 
-  public FakeTokenBuilder withAud() throws IOException {
-    String configFilePath = "secrets/configs/dev.json";
-    byte[] jsonData = Files.readAllBytes(Paths.get(configFilePath));
-    JsonObject configJson = new JsonObject(new String(jsonData, "UTF-8"));
-    JsonArray modules = configJson.getJsonArray("modules");
-    for (Object module : modules) {
-      if (module instanceof JsonObject) {
-        JsonObject moduleJson = (JsonObject) module;
-        if ("ogc.rs.authenticator.AuthenticationVerticle".equals(moduleJson.getString("id"))
-            && moduleJson.containsKey("audience")) {
-          this.aud = moduleJson.getString("audience");
-          LOGGER.debug("Audience Value : {} ", aud);
-          break;
-        }
-      }
-    }
-    return this;
-  }
-
-  public FakeTokenBuilder withRole(String role) {
-    this.role = role;
-    return this;
-  }
-
-  public FakeTokenBuilder withResource(UUID resourceId, UUID resourceGroupId) {
+  /**
+   * Sets the 'iid' and 'rg' claims for the JWT token.
+   *
+   * @param resourceId UUID representing the resource ID
+   * @param resourceGroupId UUID representing the resource group ID
+   * @return FakeTokenBuilder instance
+   */
+  public FakeTokenBuilder withResourceAndRg(UUID resourceId, UUID resourceGroupId) {
     this.iid = "ri:" + resourceId;
     this.rg = resourceGroupId;
     return this;
   }
 
-  public FakeTokenBuilder withResourceServer(String resourceServerId) {
-    this.iid = "rs:" + resourceServerId;
+  public FakeTokenBuilder withResource(UUID resourceId) {
+    this.iid = "ri:" + resourceId;
     return this;
   }
 
+  /**
+   * Sets the 'iid' claim for the JWT token for a resource server.
+   * Its same as server name and derived from config.
+   *
+   * @return FakeTokenBuilder instance
+   */
+  public FakeTokenBuilder withResourceServer() {
+    this.iid = "rs:" + aud;
+    return this;
+  }
+
+  /**
+   * Sets the 'role' claim to 'consumer'.
+   *
+   * @return FakeTokenBuilder instance
+   */
+  public FakeTokenBuilder withRoleConsumer() {
+    this.role = "consumer";
+    return this;
+  }
+
+  /**
+   * Sets the 'role' claim to 'provider'.
+   *
+   * @return FakeTokenBuilder instance
+   */
+  public FakeTokenBuilder withRoleProvider() {
+    this.role = "provider";
+    return this;
+  }
+
+  /**
+   * Sets the 'did' (delegate ID) and 'drl' (delegate role) claims for the JWT token.
+   *
+   * @param did UUID representing the delegate ID
+   * @param drl String representing the delegate role ('provider' or 'consumer')
+   * @return FakeTokenBuilder instance
+   * @throws IllegalArgumentException if an invalid 'drl' value is provided
+   */
   public FakeTokenBuilder withDelegate(UUID did, String drl) {
-    if (!"provider".equalsIgnoreCase(role) && !"consumer".equalsIgnoreCase(role)) {
+    if ("provider".equalsIgnoreCase(drl) || "consumer".equalsIgnoreCase(drl)) {
       this.did = did;
       this.drl = drl;
       this.role = "delegate";
     } else {
-      throw new IllegalArgumentException("Invalid role: Role must be 'delegate'");
+      throw new IllegalArgumentException("Invalid drl value : It should be provider or delegate");
     }
 
     return this;
   }
 
+  /**
+   * Builds and returns the signed JWT token based on the configured claims.
+   *
+   * @return String representing the serialized signed JWT token
+   * @throws RuntimeException if an error occurs during JWT token creation
+   */
   public String build() {
 
     LOGGER.debug("building the token");
@@ -125,17 +187,28 @@ public class FakeTokenBuilder {
         throw new IllegalArgumentException("Role or IID is not set");
       }
 
-      JWTClaimsSet claimsSet =
+      JWTClaimsSet.Builder claimsSetBuilder =
           new JWTClaimsSet.Builder()
-              .subject(sub)
-              .issuer("fakeauth.ugix.io")
+              .subject(String.valueOf(sub))
+              .issuer(iss)
               .audience(aud)
               .expirationTime(expirationTime)
               .issueTime(currentTime)
               .claim("iid", iid)
               .claim("role", role)
-              .claim("cons", Map.of("access", List.of("api")))
-              .build();
+              .claim("cons", JSONObjectUtils.parse(cons.toString()));
+
+      // Conditionally add fields based on role
+      if ("delegate".equals(role)) {
+        claimsSetBuilder.claim("drl", drl);
+        claimsSetBuilder.claim("did", did);
+      }
+
+      if (rg!= null) {
+        claimsSetBuilder.claim("rg", rg);
+      }
+
+      JWTClaimsSet claimsSet = claimsSetBuilder.build();
       SignedJWT signedJWT =
           new SignedJWT(
               new JWSHeader.Builder(JWSAlgorithm.ES256).type(JOSEObjectType.JWT).build(),
