c91765baf950b433061286e0637f271b2a017a9a
gopal-mahajan <gopal.mahajan@datakaveri.org>
2024-05-12 20:56:33 +0530

    test-for-processes-endpoint

---
+++ src/test/java/ogc/rs/processes/RestAssuredConfigExtension.java
@@ -0,0 +1,39 @@
+package ogc.rs.processes;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import static io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails;
+import static io.restassured.RestAssured.proxy;
+
+public class RestAssuredConfigExtension implements BeforeAllCallback {
+
+  @Override
+  public void beforeAll(ExtensionContext context) {
+    String testHost = System.getProperty("intTestHost");
+
+    if (testHost != null) {
+      RestAssured.baseURI = "http://" + testHost;
+    } else {
+      RestAssured.baseURI = "http://localhost";
+    }
+
+    String testPort = System.getProperty("intTestPort");
+
+    if (testPort != null) {
+      RestAssured.port = Integer.parseInt(testPort);
+    } else {
+      RestAssured.port = 8443;
+    }
+
+    String proxyHost = System.getProperty("intTestProxyHost");
+    String proxyPort = System.getProperty("intTestProxyPort");
+
+    if (proxyHost != null && proxyPort != null) {
+      proxy(proxyHost, Integer.parseInt(proxyPort));
+    }
+
+    enableLoggingOfRequestAndResponseIfValidationFails();
+  }
+}
