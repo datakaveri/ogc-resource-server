047a6cad38d719d2cb7648eb48496dc5e51301fa
gopal-mahajan <gopal.mahajan@datakaveri.org>
2024-05-13 12:47:18 +0530

    minor-refactor

--- src/test/java/ogc/rs/processes/ProcessesTestIT.java
+++ src/test/java/ogc/rs/processes/ProcessesTestIT.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static junit.framework.Assert.assertEquals;
+import static ogc.rs.common.Constants.DESCRIPTION_KEY;
+import static ogc.rs.common.Constants.INVALID_ENDPOINT_ERROR;
+import static ogc.rs.common.Constants.NOT_FOUND;
+import static ogc.rs.common.Constants.TYPE_KEY;
 
 /** Integration tests for the Processes endpoint. */
 @ExtendWith(RestAssuredConfigExtension.class)
@@ -20,7 +24,6 @@ public class ProcessesTestIT {
   @Description("Success: get processes")
   public void testGetProcessesSuccess() {
     Response response = requestSpecification.get(endpoint);
-    System.out.println("sattys " + response.statusCode());
     assertEquals(200, response.statusCode());
   }
 
@@ -33,9 +36,9 @@ public class ProcessesTestIT {
   public void testGetProcessesFail() {
     endpoint = endpoint.concat("makingEndpointInvalid");
     Response response = requestSpecification.get(endpoint);
-    String typeFromResponse = response.body().path("description");
+    String typeFromResponse = response.body().path(DESCRIPTION_KEY);
     assertEquals(404, response.statusCode());
-    assertEquals("API / Collection not found", typeFromResponse);
+    assertEquals(INVALID_ENDPOINT_ERROR, typeFromResponse);
   }
 
   /** Test to verify successful retrieval of a specific process. */
@@ -48,7 +51,7 @@ public class ProcessesTestIT {
     String idFromResponse = response.then().extract().body().path("processes[0].id");
 
     assertEquals(200, response.statusCode());
-    assertEquals("cc0eb191-7f66-4663-8afa-cfd644de5839", idFromResponse);
+    assertEquals(EXISTING_ID, idFromResponse);
   }
 
   /** Test to verify failure when trying to retrieve a non-existing process. */
@@ -58,8 +61,8 @@ public class ProcessesTestIT {
     String NON_EXISTING_ID = "cc0eb191-7f66-4663-8afa-cfd644de5830";
     endpoint = endpoint.concat("/").concat(NON_EXISTING_ID);
     Response response = requestSpecification.get(endpoint);
-    String typeFromResponse = response.body().path("type");
+    String typeFromResponse = response.body().path(TYPE_KEY);
     assertEquals(404, response.statusCode());
-    assertEquals("Not Found", typeFromResponse);
+    assertEquals(NOT_FOUND, typeFromResponse);
   }
 }
