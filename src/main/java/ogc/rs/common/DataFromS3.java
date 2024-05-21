25cf4fdce491bc7448e4e66ea2eae5e622efa0a1
gopal-mahajan <gopal.mahajan@datakaveri.org>
2024-05-21 16:48:16 +0530

    bug-log-added

--- src/main/java/ogc/rs/common/DataFromS3.java
+++ src/main/java/ogc/rs/common/DataFromS3.java
@@ -53,7 +53,7 @@ public class DataFromS3 {
             response.complete(res);
             return response.future();
           } else {
-            LOGGER.error("Internal Server Error, Something went wrong here. {} {}",res.statusCode());
+            LOGGER.error("Internal Server Error, Something went wrong here. {},{},{},{}",res.statusCode(),httpMethod,url.toString(),res.body().result().toString());
             response.fail(new OgcException(500, "Internal Server Error", "Internal Server Error"));
             return response.future();
           }
