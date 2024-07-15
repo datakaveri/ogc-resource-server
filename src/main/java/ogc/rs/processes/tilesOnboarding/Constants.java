773c8dfc7456993a9e94f8b87832926d02e5ef8c
DivyaSreeMunagavalasa <divya.sree@datakaveri.org>
2024-07-15 11:53:01 +0530

    Adding collection existence check and collection type check

--- src/main/java/ogc/rs/processes/tilesOnboarding/Constants.java
+++ src/main/java/ogc/rs/processes/tilesOnboarding/Constants.java
@@ -1,6 +1,11 @@
 package ogc.rs.processes.tilesOnboarding;
 
 public class Constants {
+    // Query to check if the collection exists in the collection_details table
+    public static final String CHECK_COLLECTION_EXISTENCE_QUERY =
+    "SELECT EXISTS (SELECT 1 FROM collection_details WHERE collection_id = $1)";
+    // Query to get the collection type from the collection_type table
+    public static String GET_COLLECTION_TYPE_QUERY = "SELECT type FROM collection_type WHERE collection_id = $1";
     public static final String START_TILES_ONBOARDING_PROCESS = "Starting Tiles Onboarding Process";
     public static final String S3_FILE_EXISTENCE_MESSAGE = "File exists in s3 bucket";
     public static final String S3_FILE_EXISTENCE_FAIL_MESSAGE = "File does not exist in S3 or is empty.";
@@ -10,6 +15,16 @@ public class Constants {
             "Tile metadata onboarding is not applicable for feature collections.";
     public static final String COLLECTION_TYPE_CHECK_MESSAGE =
             "Collection type check complete: the collection type is suitable for tile metadata onboarding.";
+    public static final String COLLECTION_EXISTENCE_CHECK_MESSAGE =
+            "Collection existence check completed successfully. Collection is either a pure tile collection or a feature + tile collection.";
+    public static final String COLLECTION_EXISTS_MESSAGE =
+            "Collection already exists as a map or vector";
+    public static final String UNKNOWN_COLLECTION_TYPE =
+            "Unknown collection type";
+    public static final String COLLECTION_TYPE_NOT_FOUND_MESSAGE =
+            "No type found for the collection";
+    public static final String COLLECTION_EXISTENCE_CHECK_FAILURE_MESSAGE =
+            "Failed to check collection existence";
     public static final String HANDLE_FAILURE_MESSAGE =
             "Failed to update job table status to FAILED after handler failure";
     public static final String TILES_ONBOARDING_SUCCESS_MESSAGE =
