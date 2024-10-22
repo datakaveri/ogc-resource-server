9332c541b3042746635b9bc60897c81a199f329f
code-akki <akhil.pankaj@datakaveri.org>
2024-10-22 12:59:48 +0530

    Updated flyway migration scipt V16
    * Added new column `item_id` for STAC item reference
    * Udpated `unique` constraint to include `item_id` and `id` column
    * Added default value `{}::jsonb` for column `properties`

--- src/main/resources/db/migration/V16__add_stac_items_asset_table.sql
+++ src/main/resources/db/migration/V16__add_stac_items_asset_table.sql
@@ -2,16 +2,17 @@
 
 CREATE TABLE stac_items_assets (
     id UUID PRIMARY KEY DEFAULT public.gen_random_uuid(),
-    collections_id UUID REFERENCES collections_details(id) not null,
-    title VARCHAR(255) not null,
-    description varchar(500) not null,
+    collection_id UUID REFERENCES collections_details(id) not null,
+    item_id VARCHAR(255) not null,
+    title VARCHAR(255),
+    description varchar(500),
     href VARCHAR(255) not null,
     type VARCHAR(50) not null,
     size BIGINT not null,
-    role asset_role_enum[] not null,
+    roles asset_role_enum[] not null,
     properties jsonb
 );
 
-ALTER TABLE ADD	CONSTRAINT unique_stac_asset UNIQUE (stac_collections_id, title)
+ALTER TABLE stac_items_assets ADD CONSTRAINT unique_collection_stac_item_asset UNIQUE (collection_id, item_id, id);
 
 GRANT SELECT,UPDATE,INSERT ON stac_items_assets TO ${ogcUser} ;
