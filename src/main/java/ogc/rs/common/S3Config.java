0d8f64495d4a20a8568f4df237da0ce934148cf9
ThorodanBrom <bryan.robert@datakaveri.org>
2024-12-16 12:52:32 +0530

    Update S3Config class and introduce S3ConfigsHolder class
    - Simplify S3Config class to only hold the S3 config and not do anything else
    	- Add read access config option as well
    - New S3ConfigsHolder class can load configs from server config, do validation and then return a config
    as an S3Config object by searching for an identifier
    - Make both above classes Vert.x data objects
    - Add S3BucketReadAccess enum to represent open or secure read access

--- src/main/java/ogc/rs/common/S3Config.java
+++ src/main/java/ogc/rs/common/S3Config.java
@@ -2,21 +2,21 @@ package ogc.rs.common;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import io.vertx.codegen.annotations.DataObject;
+import io.vertx.codegen.json.annotations.JsonGen;
+import io.vertx.core.json.JsonObject;
 
 /**
  * Class to hold configuration options (bucket name, access key, secret key etc.) for a single S3 or
  * S3-compatible instance.
  * 
  */
+@DataObject
+@JsonGen
 public class S3Config {
   
-  public static final String BUCKET_CONF_OP = "s3Bucket";
-  public static final String REGION_CONF_OP = "s3Region";
-  public static final String ENDPOINT_CONF_OP = "s3Endpoint";
-  public static final String ACCESS_KEY_CONF_OP = "s3AccessKey";
-  public static final String SECRET_KEY_CONF_OP = "s3SecretKey";
-  public static final String PATH_BASED_ACC_CONF_OP = "s3PathBasedAccess";
-  
+  private static final Boolean DEFAULT_IS_PATH_BASED_ACCESS = false;
+
   private String bucket;
   private String region;
 
@@ -24,20 +24,11 @@ public class S3Config {
 
   private String accessKey;
   private String secretKey;
+  
+  private S3BucketReadAccess readAccess;
 
   private Boolean pathBasedAccess;
   private Boolean https;
-  
-  private S3Config(Builder b) {
-    super();
-    this.bucket = b.bucket;
-    this.region = b.region;
-    this.endpoint = b.endpoint;
-    this.accessKey = b.accessKey;
-    this.secretKey = b.secretKey;
-    this.pathBasedAccess = b.pathBasedAccess;
-    this.https = b.endpoint.startsWith("https");
-  }
   
   /**
    * Returns the configured bucket name.
@@ -84,7 +75,7 @@ public class S3Config {
   public String getSecretKey() {
     return secretKey;
   }
-
+  
   /**
    * Returns whether the S3 instance is configured to operate with <a href=
    * "https://docs.aws.amazon.com/AmazonS3/latest/userguide/VirtualHosting.html#path-style-access">path-based
@@ -107,121 +98,57 @@ public class S3Config {
     return https;
   }
 
-  /**
-   * Class used to build an instance of {@link S3Config}.
-   */
-  public static class Builder {
-    private static final Boolean DEFAULT_IS_PATH_BASED_ACCESS = false;
+  public Boolean getPathBasedAccess() {
+    return pathBasedAccess;
+  }
+
+  public void setPathBasedAccess(Boolean pathBasedAccess) {
+    this.pathBasedAccess = pathBasedAccess;
+  }
+
+  public Boolean getHttps() {
+    return https;
+  }
+
+  public void setHttps(Boolean https) {
+    this.https = https;
+  }
+
+  public void setBucket(String bucket) {
+    this.bucket = bucket;
+  }
+
+  public void setRegion(String region) {
+    this.region = region;
+  }
+
+  public void setEndpoint(String endpoint) {
+    this.endpoint = endpoint;
+  }
+
+  public void setAccessKey(String accessKey) {
+    this.accessKey = accessKey;
+  }
+
+  public void setSecretKey(String secretKey) {
+    this.secretKey = secretKey;
+  }
   
-    private String bucket;
-    private String region;
-    private String endpoint;
-    private String accessKey;
-    private String secretKey;
-    private Boolean pathBasedAccess = DEFAULT_IS_PATH_BASED_ACCESS;
+  public S3BucketReadAccess getReadAccess() {
+    return readAccess;
+  }
 
-    /**
-     * Set the bucket name for the S3 instance.
-     * 
-     * @param bucket
-     * @return
-     */
-    public Builder bucket(String bucket) {
-      this.bucket = bucket;
-      return this;
-    }
+  public void setReadAccess(S3BucketReadAccess readAccess) {
+    this.readAccess = readAccess;
+  }
 
-    /**
-     * Set the region for the S3 instance.
-     * 
-     * @param region
-     * @return
-     */
-    public Builder region(String region) {
-      this.region = region;
-      return this;
-    }
+  public S3Config(JsonObject json) {
+    S3ConfigConverter.fromJson(json, this);
+  }
 
-    /**
-     * Set the endpoint for the S3 instance. The endpoint must be in the format
-     * [http/https]://[domain]:[port (optional)]
-     * 
-     * @param endpoint
-     * @return
-     */
-    public Builder endpoint(String endpoint) {
-      this.endpoint = endpoint;
-      return this;
-    }
-    
-    /**
-     * Set the access key for the S3 instance.
-     * 
-     * @param accessKey
-     * @return
-     */
-    public Builder accessKey(String accessKey) {
-      this.accessKey = accessKey;
-      return this;
-    }
-    
-    /**
-     * Set the secret key for the S3 instance.
-     * 
-     * @param secretKey
-     * @return
-     */
-    public Builder secretKey(String secretKey) {
-      this.secretKey = secretKey;
-      return this;
-    }
-
-    /**
-     * Set whether the S3 instance is configured to operate with <a href=
-     * "https://docs.aws.amazon.com/AmazonS3/latest/userguide/VirtualHosting.html#path-style-access">path-based
-     * access</a>. If the value is false <b>or is not set</b>, then it is considered to be <a href=
-     * "https://docs.aws.amazon.com/AmazonS3/latest/userguide/VirtualHosting.html#virtual-hosted-style-access">virtual-hosting
-     * based access</a>.
-     * 
-     * @param pathBasedAccess
-     * @return
-     */
-    public Builder pathBasedAccess(Boolean pathBasedAccess) {
-      if (pathBasedAccess == null) {
-        return this; // use default
-      }
-
-      this.pathBasedAccess = pathBasedAccess;
-      return this;
-    }
-    
-    /**
-     * Build the {@link S3Config} object.
-     * 
-     * @return
-     */
-    public S3Config build() {
-      validate();
-      return new S3Config(this);
-    }
-    
-    private void validate() throws IllegalArgumentException {
-      if (bucket == null || region == null || endpoint == null || accessKey == null
-          || secretKey == null) {
-        throw new IllegalArgumentException(
-            "Failed to initialize S3 config : atleast one of bucket, region, endpoint, access key, secret key is null");
-      }
-
-      try {
-        URI s = new URI(endpoint);
-        if (!"https".equals(s.getScheme()) && !"http".equals(s.getScheme())) {
-          throw new IllegalArgumentException("Failed to initialize S3 config : endpoint does not have http/https");
-        }
-
-      } catch (URISyntaxException e) {
-        throw new IllegalArgumentException("Failed to initialize S3 config : endpoint is invalid");
-      }
-
-    }
+  public JsonObject toJson() {
+    JsonObject json = new JsonObject();
+    S3ConfigConverter.toJson(this, json);
+    return json;
   }
 }
