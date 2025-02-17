f730abd29b17d62b08558ce14edcfeba556e3bd8
ThorodanBrom <bryan.robert@datakaveri.org>
2025-02-17 11:49:37 +0530

    Update migration for `s3_bucket_id` column
    - Remove the part of the migration that removes the DEFAULT constraint - this is affecting
    other PRs
    - Will add that migration later

--- src/main/resources/db/migration/V19_1__Add_s3_bucket_id_column_to_all_tables_that_use_S3.sql
+++ src/main/resources/db/migration/V19_1__Add_s3_bucket_id_column_to_all_tables_that_use_S3.sql
@@ -4,10 +4,3 @@ ALTER TABLE collection_coverage ADD COLUMN s3_bucket_id VARCHAR DEFAULT 'default
 ALTER TABLE collections_enclosure ADD COLUMN s3_bucket_id VARCHAR DEFAULT 'default' NOT NULL;
 ALTER TABLE stac_items_assets ADD COLUMN s3_bucket_id VARCHAR DEFAULT 'default' NOT NULL;
 ALTER TABLE stac_collections_assets ADD COLUMN s3_bucket_id VARCHAR DEFAULT 'default' NOT NULL;
-
--- removing the default constraint 
-ALTER TABLE tilematrixsets_relation ALTER COLUMN s3_bucket_id DROP DEFAULT;
-ALTER TABLE collection_coverage ALTER COLUMN s3_bucket_id DROP DEFAULT;
-ALTER TABLE collections_enclosure ALTER COLUMN s3_bucket_id DROP DEFAULT;
-ALTER TABLE stac_items_assets ALTER COLUMN s3_bucket_id DROP DEFAULT;
-ALTER TABLE stac_collections_assets ALTER COLUMN s3_bucket_id DROP DEFAULT;
