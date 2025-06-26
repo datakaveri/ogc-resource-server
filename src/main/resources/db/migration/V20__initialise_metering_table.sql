de9e0d47e883fc280da6637058f77976fff0a0df
DivyaSreeMunagavalasa <divya.sree@datakaveri.org>
2025-06-26 16:27:52 +0530

    grant usage on metering id sequence

--- src/main/resources/db/migration/V20__initialise_metering_table.sql
+++ src/main/resources/db/migration/V20__initialise_metering_table.sql
@@ -21,3 +21,4 @@ CREATE INDEX IF NOT EXISTS idx_metering_user_collection_time
 
 ALTER TABLE metering OWNER TO ${flyway:user};
 GRANT SELECT,INSERT,UPDATE,DELETE ON TABLE metering TO ${ogcUser};
+GRANT USAGE ON SEQUENCE metering_id_seq TO ${ogcUser};
