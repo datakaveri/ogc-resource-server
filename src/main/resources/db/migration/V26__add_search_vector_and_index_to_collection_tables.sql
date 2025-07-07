9eab013c08f65cb20184509d64ede9e0bdeaea9e
Tanvi Prasad <tanvi.prasad@datakaveri.org>
2025-07-07 12:11:24 +0530

    fixed the migration script

--- src/main/resources/db/migration/V26__add_search_vector_and_index_to_collection_tables.sql
+++ src/main/resources/db/migration/V26__add_search_vector_and_index_to_collection_tables.sql
@@ -14,19 +14,19 @@ BEGIN
     LIMIT 1;
 
     sql := format(
-        'ALTER TABLE public."%I" ADD COLUMN search_vector tsvector',
+        'ALTER TABLE public.%I ADD COLUMN search_vector tsvector',
         record_table
     );
     EXECUTE sql;
 
     sql := format(
-        'UPDATE public."%I" SET search_vector = to_tsvector(''english'', coalesce(title, '''') || '' '' || coalesce(description, '''') || '' '' || array_to_string(keywords, '' ''))',
+        'UPDATE public.%I SET search_vector = to_tsvector(''english'', coalesce(title, '''') || '' '' || coalesce(description, '''') || '' '' || array_to_string(keywords, '' ''))',
         record_table
     );
     EXECUTE sql;
 
     sql := format(
-        'CREATE INDEX IF NOT EXISTS idx_records_search_vector ON public."%I" USING GIN (search_vector)',
+        'CREATE INDEX IF NOT EXISTS idx_records_search_vector ON public.%I USING GIN (search_vector)',
         record_table
     );
     EXECUTE sql;
