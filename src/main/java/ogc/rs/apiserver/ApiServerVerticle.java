e5d38edb49584a1c33ace4b2826f0bf929096610
Tanvi Prasad <tanvi.prasad@datakaveri.org>
2025-10-06 16:02:45 +0530

    datetime added as a constant

--- src/main/java/ogc/rs/apiserver/ApiServerVerticle.java
+++ src/main/java/ogc/rs/apiserver/ApiServerVerticle.java
@@ -2121,9 +2121,6 @@ public class ApiServerVerticle extends AbstractVerticle {
                     if (requestBody.containsKey("crs")) {
                         existingCollection.put("crs", requestBody.getString("crs"));
                     }
-                    if (requestBody.containsKey("datetime")) {
-                        existingCollection.put("datetime", requestBody.getString("datetime"));
-                    }
                     if (requestBody.containsKey("license")) {
                         existingCollection.put("license", requestBody.getString("license"));
                     }
